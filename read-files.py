e31e45baf23cb346346084ba128a2f75c9c3a021
Ervin Mamutov <employervin@gmail.com>
2017-09-30 16:30:46 +0100

    First two steps completed and commented

--- read-files.py
+++ read-files.py
@@ -42,7 +42,7 @@ import gzip
 
 # The functions (read_labels and read_images) are used to decompress, read and store a file 
 # by passing the file location as a parameter.
-# read_labels will parse the label files to add labels to the training/testing images.
+# read_labels will read the label files and return a list of labels.
 def read_labels(file):
     try:
         with gzip.open(file) as f:
@@ -54,45 +54,74 @@ def read_labels(file):
             # If parsed correctly, the values should be the same as the values expected.
             print("File:",file,"\nMagic number: \t\t%10d\nNumber of Images: \t%10d\n"%(magic_num,no_labels))
             
-            label_list = [int.from_bytes(f.read(1), byteorder="big") for i in range(no_labels)]
+            # Create a list of labels -
+            # I don't have the processing power to loop over each label/image so I just use the first
+            # n images where n = no_labels / 1000. I can assume if it works for n images, it will work for
+            # all of the images.
+            # Looping over the number of labels divided 1000 and reading in each label 1 by 1.
+            label_list = [int.from_bytes(f.read(1), byteorder="big") for i in range(int(no_labels / 1000))]
 
+            # Return the list of labels to be used in different functions.
             return label_list
     finally:
+        # Close the file after using it.
         f.close()
 
-# read_images will parse the image files to 
+# read_images will read the image files and return a list of pixels for each image.
 def read_images(file):
     try:
         with gzip.open(file) as f:
-            # *Expected to be 2051*
+            # Magic number - *Expected to be 2051*
             magic_num = int.from_bytes(f.read(4), byteorder="big")
-            # *Expected to be 60000 training files & 10000 testing files*
+            # Number of images - *Expected to be 60000 training files & 10000 testing files*
             no_images = int.from_bytes(f.read(4), byteorder="big")
-            # *Expected to be 28*
+            # Number of rows - *Expected to be 28*
             no_rows = int.from_bytes(f.read(4), byteorder="big")
-            # *Expected to be 28*
+            # Number of columns - *Expected to be 28*
             no_cols = int.from_bytes(f.read(4), byteorder="big")
             # Print out file details.
             # If parsed correctly, the values should be the same as the expected.
             print("File:",file,"\nMagic number: \t\t%10d\nNumber of Images: \t%10d\nNumber of Rows: \t%10d\nNumber of Columns: \t%10d\n" %(magic_num,no_images,no_rows,no_rows))
 
-            pixel_list = [[[int.from_bytes(f.read(1), byteorder="big")for i in range(no_cols)] for j in range(no_rows)] for k in range(3)]
+            # Create a 3D list of image pixels - 
+            # I don't have the processing power to loop over each label/image so I just use the first
+            # n images where n = no_labels / 1000. I can assume if it works for n images, it will work for
+            # all of the images.
+            # Using Pythons use of nested angle brackets and logic within the angle brackets.
+            # 1. The first angle brackets will loop over the number of images divided by 1000
+            # 2. The second angle brackets will loop over the amount of rows.
+            # 3. The third and final angle brackets will loop over the amount of columns and record the pixel value (0-255).
+            pixel_list = [[[int.from_bytes(f.read(1), byteorder="big")for i in range(no_cols)] for j in range(no_rows)] for k in range(int(no_images / 1000))]
 
-            print_image(pixel_list[2])
-
+            # Return the list of pixels to be used in different functions.
             return pixel_list
     finally:
+        # Close the file after using it.
         f.close()
 
+# print_image will recieve a list of pixels and print out a visual representation of the image to the console.
 def print_image(pixel_list):
+    # Loop over the number of rows in the pixel list.
     for i in pixel_list:
+        # Prints out a new line after each row.
+        print("\n")
+        # Loop over the number of columns in the rows
         for j in i:
-            if( j < 127):
-                print(".")
+            # If the column value is less than 128, print out "." - This represents a white pixel.
+            if( j < 128):
+                print(".", end="")
+            # Or else if the column is greater than 128, print out "#" - This represents a black pixel.
             else:
-                print("#")
+                print("#", end="")
 
-read_labels("data/train-labels-idx1-ubyte.gz")
-read_images("data/train-images-idx3-ubyte.gz")
-read_labels("data/t10k-labels-idx1-ubyte.gz")
-read_images("data/t10k-images-idx3-ubyte.gz")
+# List of labels for the training images.
+training_labels = read_labels("data/train-labels-idx1-ubyte.gz")
+# List of pixels representing the training images.
+training_images = read_images("data/train-images-idx3-ubyte.gz")
+# List of labels for the testing images.
+testing_labels = read_labels("data/t10k-labels-idx1-ubyte.gz")
+# List of pixels representing the testing images.
+testing_images = read_images("data/t10k-images-idx3-ubyte.gz")
+
+# Call the print_image function to print out the 3rd image to the screen
+print_image(training_images[2])

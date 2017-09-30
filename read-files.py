02fed5f50faf383687ba496248a26dc533b4a442
Ervin Mamutov <employervin@gmail.com>
2017-09-30 14:46:01 +0100

    Cleaning up and optimising code

--- read-files.py
+++ read-files.py
@@ -15,26 +15,55 @@ import gzip
 
 # f.close()
 
-i = 0
+# i = 0
 
-f = gzip.open('data/train-images-idx3-ubyte.gz', 'rb')
-try:
-    byte = f.read(0)
-    for i in range(4):
-        byte = f.read(4)
-        print (int.from_bytes(byte, byteorder="big"))
-    for k in range(6000):
-        for i in range(784):
-            i = i + 1
-            # Do stuff with byte.
-            byte = f.read(1)
-            if(int.from_bytes(byte, byteorder="big") > 0):
-                print("#", end=" ")
-            else:
-                print(".", end=" ")
+# f = gzip.open('data/train-images-idx3-ubyte.gz', 'rb')
+# try:
+#     byte = f.read(0)
+#     for i in range(4):
+#         byte = f.read(4)
+#         print (int.from_bytes(byte, byteorder="big"))
+#     for k in range(6000):
+#         for i in range(784):
+#             i = i + 1
+#             # Do stuff with byte.
+#             byte = f.read(1)
+#             if(int.from_bytes(byte, byteorder="big") > 0):
+#                 print("#", end=" ")
+#             else:
+#                 print(".", end=" ")
             
-            # print (int.from_bytes(byte, byteorder="big"))
-            if(i % 28 == 0):
-                print("\n")
-finally:
-    f.close()
+#             # print (int.from_bytes(byte, byteorder="big"))
+#             if(i % 28 == 0):
+#                 print("\n")
+# finally:
+#     f.close()
+
+# The functions (read_labels and read_images) are used to decompress, read and store a file 
+# by passing the file location as a parameter.
+
+# read_labels will parse the label files to add labels to the training/testing images.
+def read_labels(file):
+    try:
+        with gzip.open(file) as f:
+            # Should be 2049
+            magic_num = int.from_bytes(f.read(4), byteorder="big")
+            # Should be 60000 training file labels & 10000 testing file labels
+            no_labels = int.from_bytes(f.read(4), byteorder="big")
+    finally:
+        f.close()
+
+# read_images will parse the image files to 
+def read_images(file):
+    try:
+        with gzip.open(file) as f:
+            # Should be 2051
+            magic_num = int.from_bytes(f.read(4), byteorder="big")
+            # Should Should be 60000 training files & 10000 testing files
+            no_images = int.from_bytes(f.read(4), byteorder="big")
+            # Should be 28
+            no_rows = int.from_bytes(f.read(4), byteorder="big")
+            # Should be 28
+            no_cols = int.from_bytes(f.read(4), byteorder="big")
+    finally:
+        f.close()
